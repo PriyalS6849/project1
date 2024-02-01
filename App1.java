e05cd79ff0f82b804aa2f2b7c0767d9c776e49cb
PriyalS6849 <priyalsaxena6849@gmail.com>
2024-02-01 23:46:12 +0530

    finally 84

--- App1.java
+++ App1.java
@@ -1,7 +1,5 @@
-public class App1{
-
-    public static void main(String[] args){
-        System.out.println("Java App Pulled & Build by Jenkins");
-        System.out.println("Java App Pulled & Build by Jenkins");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
